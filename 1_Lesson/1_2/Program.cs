cf44d627d4b5309238561f2e8be446fb05cc23f6
Marina Sudakova <rubbish_bin@inbox.ru>
2022-07-08 13:34:50 +0500

    Task1_2

---
+++ 1_Lesson/1_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// проверить является ли число квадратом
+Console.WriteLine("Write a first number: ");
+string s_a = Console.ReadLine();
+int a = int.Parse(s_a);
+
+Console.WriteLine("Write a second number: ");
+string s_b = Console.ReadLine();
+int b = int.Parse(s_b);
+
+if (a == b * b)
+Console.WriteLine("Yes");
+else 
+Console.WriteLine("No");
