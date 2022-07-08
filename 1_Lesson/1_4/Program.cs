9fba34f22b9d1acf700423eacc14e59f4f4e3207
Marina Sudakova <rubbish_bin@inbox.ru>
2022-07-08 14:16:58 +0500

    Task4

---
+++ 1_Lesson/1_4/Program.cs
@@ -0,0 +1,10 @@
+﻿// вывести числа до N
+Console.WriteLine("Write a number a: ");
+int a = int.Parse(Console.ReadLine());
+// if (a < 0)
+int i = -a;
+while (i < a)
+{
+    Console.WriteLine(i);
+    i++;
+}
