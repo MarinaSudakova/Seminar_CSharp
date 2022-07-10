b71f512d2edc1c43f3e3676cb364ae8f367b5fd5
Marina Sudakova <rubbish_bin@inbox.ru>
2022-07-10 23:34:08 +0500

    Task 1_7

---
+++ 1_Lesson/1_7/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+Console.WriteLine("Write a three-digit number: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a < 100)
+Console.WriteLine("What's wrong with you, not a three-digit number!");
+
+else if (a >= 1000)
+Console.WriteLine("What's wrong with you, not a three-digit number!");
+
+else
+Console.WriteLine(a%10);
+
