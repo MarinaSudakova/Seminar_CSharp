f1f6a19c6c82a0285862040148cc66723bbca593
Marina Sudakova <rubbish_bin@inbox.ru>
2022-07-14 12:12:17 +0500

    Task 2_1 v 2

---
+++ 2_Lesson/HOMEWORK/2_1_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+void SecondDigit (string num)
+{
+   Console.WriteLine(num[1]);
+}
+
+Console.WriteLine("Write a three-digit number: ");
+string s_a = Console.ReadLine();
+
+SecondDigit (s_a);
