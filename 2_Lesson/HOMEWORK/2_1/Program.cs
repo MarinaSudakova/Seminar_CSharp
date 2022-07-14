c6728e56661644e4f82b627c96ffd2a61079f33a
Marina Sudakova <rubbish_bin@inbox.ru>
2022-07-14 11:57:37 +0500

    Task 2_1 v 1

---
+++ 2_Lesson/HOMEWORK/2_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+void SecondDigit (int num)
+{
+    if (num > 99 && num < 1000)
+    {
+        int a = num/10;
+        Console.WriteLine($"Second digit is {a % 10}");
+    }
+
+    else
+    Console.WriteLine("It is not tree-digit number");
+}
+
+Console.WriteLine("Write a three-digit number: ");
+int s_a = int.Parse(Console.ReadLine());
+
+SecondDigit (s_a);
+
