286b99fe67799ae51a5caf49296c8a1b068be5f3
Marina Sudakova <rubbish_bin@inbox.ru>
2022-07-14 12:39:38 +0500

    Task 2_2 v 1

---
+++ 2_Lesson/HOMEWORK/2_2_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число и 
+// удаляет вторую цифру этого числа.
+
+void CutNum (string num)
+{
+    Console.WriteLine(num[0],num[2]);
+}
+
+int number = new Random().Next(100,1000);
+string s_a = number.ToString();
+
+Console.Write($"{number} change to ");
+CutNum (s_a);
