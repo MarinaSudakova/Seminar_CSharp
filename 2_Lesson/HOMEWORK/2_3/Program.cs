f984c8b60ef897888823c9faf01ab021c8ae7cd3
Marina Sudakova <rubbish_bin@inbox.ru>
2022-07-14 13:20:38 +0500

    Task 2_3

---
+++ 2_Lesson/HOMEWORK/2_3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+void ThirdDigit (int num)
+{
+    if (num < 100)
+    Console.WriteLine("Third digit is not found");
+
+    else
+    {
+      while (num > 999)
+         num = num/10;
+      Console.WriteLine($"Third digit is {num % 10}");
+    }
+}
+
+Console.WriteLine("Write a three-digit number: ");
+int s_a = int.Parse(Console.ReadLine());
+
+ThirdDigit (s_a);
+
