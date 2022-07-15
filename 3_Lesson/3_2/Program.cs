53dcbf4176b60411099785ab49c12390a874b2a2
Marina Sudakova <rubbish_bin@inbox.ru>
2022-07-15 13:00:12 +0500

    Task 3_2

---
+++ 3_Lesson/3_2/Program.cs
@@ -0,0 +1,9 @@
+﻿// Вывод расстояния между точками
+
+
+double Distance (double x1, double y1, double x2, double y2)
+{
+    return Math.Round(Math.Sqrt(Math.Pow(x1 - x2,2) + Math.Pow(y1 - y2,2)),4);
+}
+
+Console.WriteLine (Distance(3, 6, 2, 1));
