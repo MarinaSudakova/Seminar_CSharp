ea44f02890a0e20d40f2165b651da1a26ebcc53d
Marina Sudakova <rubbish_bin@inbox.ru>
2022-07-15 13:10:24 +0500

    Task 3_3

---
+++ 3_Lesson/3_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+
+
+void Quarter (int x)
+{
+    int index = 1;
+    while (index <= x)
+    {
+        Console.Write ($"{Math.Pow(index,2)} ");
+        index++;
+    }
+}
+
+Quarter (7);
+
