d3a3d3718125bf0f52a5e0ae22887179c75fdf3d
Marina Sudakova <rubbish_bin@inbox.ru>
2022-07-18 20:08:30 +0500

    Task 3_23

---
+++ 3_Lesson/HOMEWORK/3_23/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая
+// принимает на вход число (N) и выдаёт
+// выдаёт таблицу кубов чисел от 1 до N.
+
+
+void Cube (int x)
+{
+    int index = 1;
+    while (index <= x)
+    {
+        Console.WriteLine ($"{index} -> {Math.Pow(index,3)}");
+        index++;
+    }
+}
+
+Console.WriteLine ("Write a number: ");
+int s_a = int.Parse(Console.ReadLine());
+Cube (s_a);
+
