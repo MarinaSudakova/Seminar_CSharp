09e198509c8be2258e7037b6557f0413ed885c95
Marina Sudakova <rubbish_bin@inbox.ru>
2022-07-18 20:39:28 +0500

    Task 3_19 v 2

---
+++ 3_Lesson/HOMEWORK/3_19_2/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход 
+// пятизначное число и проверяет, является ли оно палиндромом.
+
+void Pallindrom (string num)
+{
+    int length = num.Length;
+    if (num.Length == 5)
+    {
+        if (num[0] == num[4] && num[1] == num[3])
+        Console.WriteLine("Yes");
+
+        else
+        Console.WriteLine("No");
+    }
+    else
+    Console.WriteLine("Not a five-digit number!");
+}
+
+Console.Write("Write a five-digit number: ");
+string s_a = Console.ReadLine();
+
+Pallindrom(s_a);
