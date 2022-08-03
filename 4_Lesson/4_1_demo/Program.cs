d535d431de86d0432e1f05e9a3f3522d17f28e86
Marina Sudakova <rubbish_bin@inbox.ru>
2022-08-03 19:33:36 +0500

    Task demo

---
+++ 4_Lesson/4_1_demo/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает число А 
+// и выдает сумму от 1 до А
+int SumNum (int num)
+{
+    int all_sum = 0;
+    for (int i=1; i<=num; i++)
+    {
+        all_sum += i;
+    }
+    return all_sum;
+}
+
+Console.WriteLine (SumNum(7));
+
+
