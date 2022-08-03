a9862fa89c526da91db408a2b429d2daf9809718
Marina Sudakova <rubbish_bin@inbox.ru>
2022-08-03 20:06:14 +0500

    Task 4_3

---
+++ 4_Lesson/4_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Массив из 8 элементов заполненный 0 и 1 в случаом порядке
+
+void RandomArr (int num)
+{
+    int[] array = new int[num];
+    for (int i=0; i<num; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+        Console.Write($"{array[i]} ");
+    }
+}
+
+RandomArr(8);
+
