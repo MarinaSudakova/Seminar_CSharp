0ea240c8fc5eefdcbfe602cc763d84984b0ea50f
Marina Sudakova <rubbish_bin@inbox.ru>
2022-08-05 00:55:26 +0500

    Task 4_6

---
+++ 4_Lesson/HOMEWORK/4_6/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+
+int FillArray(int[] num)
+{
+        for (int i=0; i<8; i++)
+    {
+        num[i] = new Random().Next(1, 30);
+    }
+    return num;
+}
+
+void ShowArray(int[] array)
+{
+    for (int ind = 0; ind < array.Length; ind++)
+    {
+        Console.Write ($"{array[ind]} ");
+    }
+}
+
+int arr_1 = FillArray();
+
+ShowArray(arr_1);
