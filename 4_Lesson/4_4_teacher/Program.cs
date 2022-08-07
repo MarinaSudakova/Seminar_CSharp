2656a75071587bbc10108ee03d387f72f9a43d92
Marina Sudakova <rubbish_bin@inbox.ru>
2022-08-07 15:28:33 +0500

    teacher version

---
+++ 4_Lesson/4_4_teacher/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран
+
+int [] FillArray()
+{
+    int size = 8;
+    int[] array = new int[size];
+    for (int i=0; i<size; i++)
+    {
+        array[i] = new Random().Next(1, 30);
+    }
+    return array;
+}
+
+
+void ShowArray(int[] coll)
+{
+    for (int ind = 0; ind < coll.Length; ind++)
+    {
+        Console.Write ($"{coll[ind]} ");
+    }
+}
+
+int[] arr_1 = FillArray();
+ShowArray(arr_1);
+
+
