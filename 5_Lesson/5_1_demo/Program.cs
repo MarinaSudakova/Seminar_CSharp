1b646d3f1681ea71bf45f2ab2bc4a688fb87dd7c
Marina Sudakova <rubbish_bin@inbox.ru>
2022-08-07 15:53:01 +0500

    Task 5_1_demo

---
+++ 5_Lesson/5_1_demo/Program.cs
@@ -0,0 +1,42 @@
+﻿// Массив из 12 цифр положительных и отрицательных
+// по отдельносьти суммы положительных и отрицательных
+
+int [] MassNums(int size, int from, int to)
+{
+    int[] array = new int[size];
+    for (int i=0; i<size; i++)
+    {
+        array[i] = new Random().Next(from, to);
+    }
+    return array;
+}
+
+
+void ShowArray(int[] coll)
+{
+    for (int ind = 0; ind < coll.Length; ind++)
+    {
+        Console.Write ($"{coll[ind]} ");
+    }
+}
+
+void SumPosNeg (int[] arr)
+{
+    int pos, neg;
+    pos = neg = 0;
+
+    for (int j = 0; j<arr.Length; j++)
+    {
+        if (arr[j] >=0)
+            pos += arr[j];
+        else    
+            neg += arr[j];
+    }
+    Console.WriteLine ($"Positive: {pos}, negative: {neg}");
+}
+
+
+int[] arr_1 = MassNums(12, -9, 10);
+ShowArray(arr_1);
+SumPosNeg (arr_1);
+
