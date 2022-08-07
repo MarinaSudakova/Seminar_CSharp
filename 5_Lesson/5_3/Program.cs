b0b29b1d173c8d81bdf2c68dcd5b54d2d0afc820
Marina Sudakova <rubbish_bin@inbox.ru>
2022-08-07 16:52:49 +0500

    Task 5_3

---
+++ 5_Lesson/5_3/Program.cs
@@ -0,0 +1,40 @@
+﻿// 3. Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов 
+//массива, значения которых лежат в отрезке [10,99].
+
+int [] MassNums(int size, int from, int to)
+{
+    int[] array = new int[size];
+    for (int i=0; i<size; i++)
+    {
+        array[i] = new Random().Next(from, to);
+    }
+    return array;
+}
+
+
+void ShowArray(int[] coll)
+{
+    for (int ind = 0; ind < coll.Length; ind++)
+    {
+        Console.Write ($"{coll[ind]} ");
+    }
+    Console.WriteLine();
+}
+
+int Find (int[] arr)
+{
+    int count = 0;
+    for (int j = 0; j<arr.Length; j++)
+    {
+        if (arr[j] >= 10 && arr[j]<=99)
+        count+=1;
+    }
+    return count;
+}
+
+int[] arr_1 = MassNums(123, -9, 100);
+ShowArray(arr_1);
+Console.WriteLine (Find (arr_1));
+
+
