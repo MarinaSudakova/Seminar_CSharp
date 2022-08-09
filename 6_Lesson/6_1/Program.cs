f935865ac24570438b286e13da85f9daf90d48fc
Marina Sudakova <rubbish_bin@inbox.ru>
2022-08-09 12:47:01 +0500

    Task 6_1

---
+++ 6_Lesson/6_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// 1. Напишите программу, которая принимает на вход три числа
+//    и проверяет, может ли существовать треугольник с сторонами такой длины.
+
+
+void Triangle (int x, int y, int z)
+{
+    if (x+y > z && x+z > y && z+y > y)
+    {
+        Console.WriteLine ("Yes");
+    }
+    else
+        Console.WriteLine ("No");
+}
+
+Triangle (1, 2, 3);
+
+
