94b08dc22838b4513beb8edf2fafc15e07882662
Marina Sudakova <rubbish_bin@inbox.ru>
2022-08-11 16:43:25 +0500

    Task 6 2

---
+++ 6_Lesson/HOMEWORK/6_2/Program.cs
@@ -0,0 +1,45 @@
+﻿// Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+
+int[] FillArray (int count)
+{
+    int[] array = new int[count];
+
+    for (int i=0; i<count; i++)
+    {
+        Console.WriteLine ("Write next number: ");
+        int elem = int.Parse (Console.ReadLine());
+        array[i] = elem;
+    }
+    return array;
+}
+
+void PrintArray (int[] arr)
+{
+    for (int ind = 0; ind<arr.Length; ind++)
+    {
+        Console.Write ($"{arr[ind]} ");
+    }
+    Console.WriteLine();
+}
+
+void HowMany (int[] arr)
+{
+    int sum =0;
+    for (int ind = 0; ind<arr.Length; ind++)
+    {
+        if (arr[ind] >0)
+            sum++;
+    }
+    Console.WriteLine($"There are {sum} positive numbers");
+}
+
+Console.WriteLine ("How many numbers do you want to write? ");
+int size = int.Parse (Console.ReadLine());
+
+int[] arr_1 = FillArray (size);
+PrintArray(arr_1);
+
+HowMany (arr_1);
