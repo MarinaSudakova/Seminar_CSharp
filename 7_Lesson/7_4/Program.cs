2beb712eef5153b5b73249f7abdf37eee52e1ff9
Marina Sudakova <rubbish_bin@inbox.ru>
2022-08-12 13:17:30 +0500

    Task 7_4

---
+++ 7_Lesson/7_4/Program.cs
@@ -0,0 +1,61 @@
+﻿// // 4. Задайте двумерный массив. Введите элемент, и найдите первое его вхождение,
+//    выведите позиции по горизонтали и вертикали, или напишите, что такого элемента нет.
+
+
+
+void PrintArray (int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i=0; i< row_size; i++)
+    {
+        for (int j=0; j< column_size; j++)
+        {
+            Console.Write ($" {arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+
+int [,] MussNums (int row, int column, int from, int to)
+{
+    int [,] arr = new int[row, column];
+
+    for (int i=0; i< row; i++)
+    {
+        for (int j=0; j< column; j++)
+        {
+            arr[i, j] = new Random().Next (from,to);
+        }
+    }
+    return arr;
+}
+
+string FindNum (int[,] arr, int find)
+{
+    for (int i=0; i<arr.GetLength(0); i++)
+    {
+        for (int j = 0; j<arr.GetLength(1); j++)
+        {
+            if (arr[i, j] == find)
+                return $" {i+1} {j+1} ";
+        }
+    }
+    return "No";
+}
+
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] arr_1 = MussNums(row, column, 1, 11);
+PrintArray(arr_1);
+Console.Write("Enter the number to find: ");
+Console.WriteLine (FindNum (arr_1, int.Parse(Console.ReadLine())));
+
+
