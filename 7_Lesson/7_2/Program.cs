8910cd1835f916d089640b68e482fab2f419f3ce
Marina Sudakova <rubbish_bin@inbox.ru>
2022-08-12 12:47:00 +0500

    Task 7_2

---
+++ 7_Lesson/7_2/Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+// // 1. Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+
+void PrintArray (int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i=0; i< row_size; i++)
+    {
+        for (int j=0; j< column_size; j++)
+        {
+            Console.Write ($" {arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+
+int [,] MussNums (int row, int column, int from, int to)
+{
+    int [,] arr = new int[row, column];
+
+    for (int i=0; i< row; i++)
+    {
+        for (int j=0; j< column; j++)
+        {
+            arr[i, j] = new Random().Next (from,to);
+        }
+    }
+    return arr;
+}
+
+int[,] ReplaceElOn2ndPos2xArray(int[,] arr)
+{
+    for(int i=1; i<arr.GetLength(0); i+=2)
+    {
+        for(int j=1; j<arr.GetLength(1); j+=2)
+        {
+            arr[i,j]*=arr[i,j];
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    return(arr);
+}
+
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] arr_1 = MussNums(row, column, 1, 11);
+PrintArray(arr_1);
+ReplaceElOn2ndPos2xArray(arr_1);
+PrintArray(arr_1);
+
+
