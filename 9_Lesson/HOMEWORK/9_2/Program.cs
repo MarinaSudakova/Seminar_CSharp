43fb08423a8c74a4e7284f507aa8aada9ad6a87b
Marina Sudakova <rubbish_bin@inbox.ru>
2022-08-21 21:09:28 +0500

    HW 9_2

---
+++ 9_Lesson/HOMEWORK/9_2/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int SumNums (int a, int b)
+{
+    if (b < a) return 0;
+    return SumNums(a, b - 1) + b;
+}
+
+Console.WriteLine (SumNums(4, 8));
