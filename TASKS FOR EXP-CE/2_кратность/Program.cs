f47424b657ff6c90f0566fdf2f9508d6caf608be
Marina Sudakova <rubbish_bin@inbox.ru>
2022-08-24 14:42:39 +0500

    Multiplicity

---
+++ TASKS FOR EXP-CE/2_кратность/Program.cs
@@ -0,0 +1,17 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+void Multiplicity (int a, int b)
+{
+    if (a % b == 0)
+        Console.WriteLine ("It is ok");
+    else
+        Console.WriteLine ($" It is not ok, remainder is {a % b} ");
+}
+
+Console.WriteLine("Write a first number: ");
+int a = int.Parse (Console.ReadLine());
+
+Console.WriteLine("Write a second number: ");
+int b = int.Parse (Console.ReadLine());
+
+Multiplicity (a, b);
