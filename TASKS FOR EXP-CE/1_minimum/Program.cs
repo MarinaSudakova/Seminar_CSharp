7ad59cb0bab8b5194742a43725e3243de623dfce
Marina Sudakova <rubbish_bin@inbox.ru>
2022-08-24 14:28:37 +0500

    1_minimum

---
+++ TASKS FOR EXP-CE/1_minimum/Program.cs
@@ -0,0 +1,20 @@
+﻿// 11. Дано число из отрезка [10, 99]. 
+// Показать наибольшую цифру числа
+
+void FindMinimum (int a)
+{
+    if (a < 10 || a > 99)
+        Console.WriteLine ("Impossible");
+    else
+    {
+        if (a % 10 > a / 10)
+            Console.WriteLine ($"Biggest is {a%10} ");
+        else
+            Console.WriteLine ($"Biggest is {a/10} ");
+    }
+}
+
+Console.WriteLine("Write a number from 10 to 99: ");
+int num = int.Parse (Console.ReadLine());
+
+FindMinimum (num);
